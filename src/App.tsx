724b5278303d40a5b57af51724947c195185647f
Jaroslav Kaňka <kanka@kankaj.cz>
2023-03-01 00:59:27 +0100

    Version 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,88 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import { SetStateAction, useEffect, useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [hex, setHex] = useState<string>('0')
+    const [binaryHead, setBinaryHead] = useState<number[]>([])
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    function hexToDecimal(hex: string) {
+        return parseInt(hex, 16)
+    }
+
+    function hexToBinary(hex: string) {
+        return (parseInt(hex, 16).toString(2))
+    }
+
+    function handleChange(e: { target: { value: SetStateAction<string>; }; }) {
+        setHex(e.target.value)
+    }
+
+    useEffect(() => {
+        let decimalArray: number[] = [1]
+
+        for (let i = 1; i < hexToBinary(hex).split('').length; i++) {
+            decimalArray.push(decimalArray[decimalArray.length - 1] * 2)
+        }
+
+        decimalArray.reverse()
+        setBinaryHead(decimalArray)
+    }, [hex])
+
+    return (
+        <div className='container'>
+            <nav>
+                <h1>HEX to DEC and BIN</h1>
+            </nav>
+            <section className='user-input-section'>
+                <div className='card'>
+                    <label htmlFor='userInput'>HEX: </label>
+                    <input name='userInput' onChange={handleChange} maxLength={5} type='text'
+                           placeholder='Enter HEX number'/>
+                </div>
+            </section>
+            <section className='results-table-section'>
+                <h2>Overview</h2>
+                <div className='card'>
+                    <table className='results-table-summary'>
+                        <thead>
+                        <tr>
+                            <td>HEX</td>
+                            <td>DEC</td>
+                            <td>BIN</td>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>{(hex === '' ? '0' : hex.toString())}</td>
+                            <td>{(hex === '' ? '0' : hexToDecimal(hex))}</td>
+                            <td>{(hex === '' ? '0' : hexToBinary(hex))}</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </section>
+            <section className='visualize-table-section'>
+                <h2>Calculation visualization in Decimal</h2>
+                <div className='card'>
+                    <table className='visualization-table'>
+                        <thead>
+                        <tr>
+                            {binaryHead.map((number) => <td>{number}</td>)}
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            {hexToBinary(hex).split('').map((value) => <td>{value}</td>)}
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </section>
+            <footer>
+                Made by <a href='https://kankaj.cz' target='_blank'>Jaroslav Kaňka</a> 2023
+            </footer>
+        </div>
+    )
 }
 
 export default App
